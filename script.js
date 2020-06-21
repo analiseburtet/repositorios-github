d7177bff580402e780ffc9a86733b55211b318dd
analiseburtet <analiseburtet@gmail.com>
2020-06-21 15:40:41 -0300

    adicionar efeito hover aos links dos repositorios

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         $.get('https://api.github.com/users/torvalds/repos?page=1&per_page=20', function(data){
             let statusHTML = ""
             $.each(data, function(i, status){
-                statusHTML += '<li class="nav-item">'
+                statusHTML += '<li class="nav-item border rounded mb-1">'
                 statusHTML += '<a class="nav-link repo-link">'+ status.name +'</a>'
                 statusHTML += '</li>'
             })
@@ -17,13 +17,15 @@ $(document).ready(function(){
     $(".repo-list").on("click", "a.repo-link", function(e) {
         let repo = $( this ).text()
         buscarContribuidores(repo)
+        $(".hide").fadeIn("slow");
     })
 
 
     function buscarContribuidores(repo){
         const urlBase = 'https://api.github.com/repos/torvalds/'
         const urlRepo = urlBase + repo + '/collaborators'
-        $.get(urlRepo, {  }).done(function(data){
+        $.get(urlRepo)
+        .done(function(data){
             let statusHTML = ""
             $.each(data, function(i, status){
                 console.log(status)
