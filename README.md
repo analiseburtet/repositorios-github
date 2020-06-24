0db81d9c54bc558dedf44cca3102b2c8834657fa
analiseburtet <analiseburtet@gmail.com>
2020-06-24 10:25:28 -0300

    refatorar

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Repositórios UI 
 
-Baseada em Bootstrap 4, essa UI mostra os repositórios que tem entre 150000 e 300000 estrelas, busca até 30 issues, com a descrição de cada uma.
+Baseada em Bootstrap 4, essa UI mostra os repositórios que tem entre 150000 e 300000 estrelas, busca até 30 issues, com a descrição de cada uma e comentários(se houver). Filtra issues por closed e open. Agrupa contribuidores por quantidade de contribuições.
 
 ## Stack
 
@@ -12,5 +12,5 @@ Baseada em Bootstrap 4, essa UI mostra os repositórios que tem entre 150000 e 3
 ## Como rodar o projeto
 
 - clone o projeto;
-- Atualize o script.js de ``` GITHUB_TOKEN_HERE ``` para o seu github token;
+- Atualize o TOKEN de  ``` GITHUB_TOKEN_HERE ``` para o seu [github token](https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line);
 - Abra o index.html no browser.
--- script.js
+++ script.js
@@ -1,110 +1,176 @@
-$(document).ready(function(){
-
-    const urlBase = "https://api.github.com"
-    let state = "open"
-
-    async function buscarRepositorios(){
-        const url = urlBase + '/search/repositories?q=stars:150000..300000'
-
-        const response = await fetch(url)
-        const data = await response.json()
-        let array = data.items
-        let statusHTML = ""
-        array.forEach(i => {
-            statusHTML += '<li class="nav-item border rounded mb-1">'
-            statusHTML += '<a class="nav-link repo-link" data-repo-name="'+ i.full_name +'">'+ i.name +'</a>'
-            statusHTML += '</li>'
-        })
-        $('.repo-list').html(statusHTML)
+$(document).ready(function () {
+    const API_ENDPOINT = "https://api.github.com";
+    const TOKEN = GITHUB_TOKEN_HERE
+  
+    const createContributorsApi = (repo) =>
+      `${API_ENDPOINT}/repos/${repo}/contributors?q=contributions&order=desc`;
+  
+    const createReposApi = () =>
+      `${API_ENDPOINT}/search/repositories?q=stars:150000..300000`;
+  
+    const createIssuesApi = (repo, state) =>
+      `${API_ENDPOINT}/search/issues?q=state:${state}+repo:${repo}+type:issue`;
+  
+    const createCommentsApi = (repo, issue = 19046) =>
+      `${API_ENDPOINT}/repos/${repo}/issues/${issue}/comments`;
+  
+    async function transport(url, params = {}) {
+      const options = {
+        method: params.method || "GET",
+        headers: {
+          Authorization: `Token ${TOKEN}`,
+          Accept: "application/vnd.github.cloak-preview",
+        },
+      };
+  
+      if (params.body) {
+        options.body = params.body;
+      }
+  
+      let response;
+      try {
+        response = await fetch(url, options);
+      } catch (error) {
+        console.error(error);
+        throw new Error(error, "Error fetching data");
+      }
+  
+      if (response.ok) {
+        try {
+          return response.json();
+        } catch (error) {
+          console.error(error);
+          throw new Error(error, "Error parsing JSON data");
+        }
+      }
     }
-
-    buscarRepositorios()
-
-    $(".repo-list").on("click", "a.repo-link", function(e) {
-        let repo = $(this).attr("data-repo-name")
-        buscarContribuidores(repo)
-        buscarIssues(repo)
-        $(".hide").fadeIn("slow");
-        $(this).toggleClass('active')
-    })
-
-    $(".issues-list").on("click", "a.issue-link", function(e) {
-        $(".issue-hide").fadeIn("slow");
-    })
-
-    $("body").on("click", ".open", function(e) {
-        state = "open"
-    })
-
-    $("body").on("click", ".open", function(e) {
-        state = "closed"
-    })
-
-    async function buscarIssues(repo){
-        const headers = {
-            "Authorization": `Token GITHUB_TOKEN_HERE`
+  
+    async function buscarRepositorios() {
+      const data = await transport(createReposApi());
+      $(".repo-list").html(
+        data.items.map(
+          (i) =>
+            `<li class="nav-item nav-link border rounded mb-1">
+                <a class="nav-item nav-link repo-link" data-repo-name=${i.full_name}>
+                    ${i.name}
+                </a>
+            </li>
+            `
+        )
+      );
+    }
+  
+    buscarRepositorios();
+  
+    $(".repo-list").on("click", "a.repo-link", function (e) {
+      let repo = $(this).attr("data-repo-name");
+      buscarContribuidores(repo);
+      buscarIssues(repo, "open");
+      $(".hide").fadeIn("slow");
+      $(this).parent().addClass('active').siblings().removeClass('active');
+    });
+  
+    $(".issues-list").on("click", "a.issue-link", function (e) {
+      $(".issue-hide").fadeIn("slow");
+    });
+  
+    async function buscarIssues(repo, state) {
+      console.log(repo, state);
+      const data = await transport(createIssuesApi(repo, state));
+      console.log(data);
+  
+      $(".issues-list").html(
+        data.items.map(
+          (i) =>
+            `
+      <li class="nav-item border rounded mb-1">
+          <a class="nav-link issue-link" data-issue-number=${i.number}>
+              ${i.title}
+          </a>
+          <div class="nav-link">
+              ${i.body}
+          </div>
+      </li>
+      `
+        )
+      );
+  
+      $("body").on("click", ".issue-link", async function (e) {
+        let issueNumber = $(this).attr("data-issue-number");
+        await buscarComentarios(repo, issueNumber);
+        $(this).toggleClass("active");
+      });
+    }
+  
+    async function buscarContribuidores(repo) {
+      const data = await transport(createContributorsApi(repo));
+  
+      const finalData = {
+        [`100-200`]: [],
+        [`200-500`]: [],
+        [`500+`]: [],
+      };
+      data.forEach((dataPoint) => {
+        if (dataPoint.contributions > 100 && dataPoint.contributions < 200) {
+          finalData["100-200"].push(dataPoint);
+        } else if (
+          dataPoint.contributions >= 200 &&
+          dataPoint.contributions < 500
+        ) {
+          finalData["200-500"].push(dataPoint);
+        } else if (dataPoint.contributions > 500) {
+          finalData["500+"].push(dataPoint);
         }
-        const url = urlBase + "/search/issues?q=state:"+ state + "+repo:" + repo + "+type:issue"
-        const response = await fetch(url , {
-            "method": "GET",
-            "headers": headers
-        })
-        const data = await response.json()
-        let array = data.items
-        let statusHTML = ""
-        array.forEach(i => {
-            statusHTML += '<li class="nav-item border rounded mb-1">'
-            statusHTML += '<a class="nav-link issue-link" data-issue-number="'+ i.number +'">'+ i.title +'</a>'
-            statusHTML += '<div class="nav-link">'+ i.body +'</div>'
-            statusHTML += '</li>'
-        })
-        $('.issues-list').html(statusHTML)
-
-        $("body").on("click", ".issue-link", function(e) {
-            let issueNumber = $(this).attr("data-issue-number")
-            buscarComentarios(repo, issueNumber)
-            $(this).toggleClass('active')
-        })
-    }  
-
-    async function buscarContribuidores(repo){
-        const headers = {
-            "Authorization": `Token GITHUB_TOKEN_HERE`,
-            "Accept": "application/vnd.github.cloak-preview"
-        }
-        const url = urlBase + "/repos/" + repo + "/contributors?q=contributions&order=desc"
-        const response = await fetch(url , {
-            "method": "GET",
-            "headers": headers
-        })
-        const data = await response.json()
-        let statusHTML = ""
-        data.forEach(i => {
-            statusHTML += '<li class="nav-item border rounded mb-1">'
-            statusHTML += '<a class="nav-link">'+ i.login +'</a>'
-            statusHTML += '</li>'
-        })
-        $('.constributors-list').html(statusHTML)
-    }  
-
-    async function buscarComentarios(repo){
-        const headers = {
-            "Authorization": `Token GITHUB_TOKEN_HERE`,
-            "Accept": "application/vnd.github.cloak-preview" 
-        }
-        const url = urlBase + "/repos/" + repo + "/issues/"+ "19046" +"/comments"
-        const response = await fetch(url , {
-            "method": "GET",
-            "headers": headers
-        })
-        const data = await response.json()
-        let statusHTML = ""
-
-        data.forEach(i => {
-            statusHTML += '<div class="nav-item nav-link border rounded mb-1">'
-            statusHTML += i.body
-            statusHTML += '</div>'
-        })
-        $('.comments-list').html(statusHTML)
+      });
+  
+      let statusHTML = "";
+      Object.entries(finalData).forEach(([key, values]) => {
+        statusHTML += `
+          <div class="col-sm">
+              <div>${key}</div>
+              ${values.map(
+                (value) =>
+                  `<li class="nav-item border rounded mb-1">
+                      <a class="nav-link">${value.login}</a>
+                  </li>
+                  `
+              )}
+          </div>
+          `;
+      });
+  
+      $(".constributors-list").html(`
+          ${statusHTML} 
+          <div class="container row issues">
+            <h1>Issues</h1>
+            <div>
+            <button value="${repo}" id="open" class="btn btn-success">
+                Open
+            </button>
+            <button value="${repo}" id="closed" class="btn btn-danger">
+                Closed
+            </button>
+            </div>
+          </div>
+  `);
+      $("#open").click(async (event) => {
+        await buscarIssues(event.target.value, "open");
+      });
+      $("#closed").click(async (event) => {
+        await buscarIssues(event.target.value, "closed");
+      });
+    }
+  
+    async function buscarComentarios(repo) {
+      const data = await transport(createCommentsApi(repo, 19046));
+      $(".comments-list").html(
+        data.map(
+          (i) =>
+            `<div class="nav-item nav-link border rounded mb-1">
+          ${i.body}
+          </div>
+        `
+        )
+      );
     }
 });
